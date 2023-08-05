437d1cf33a33d07decd8e641b9183fd59393135e
Sergey Storchay <r8@r8.com.ua>
2023-08-05 18:02:06 +0300

    refactor: Update arrows color

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,3 +1,3 @@
-export const ARROW_COLOR = "rgba(0, 128, 0, 0.75)";
+export const ARROW_COLOR = "rgb(0, 128, 0)";
 export const SQUARE_MARK_COLOR = "rgba(0, 128, 0, 0.75)";
 export const SQUARE_HIGHLIGHT_COLOR = "rgba(155, 199, 0, 0.5)";
